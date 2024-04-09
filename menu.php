d00490e2548a8c5bdba8ed5b16b55fc8fb740a03
siran <siranro28@gmail.com>
2024-04-09 15:38:51 +0530

    initialize repo

---
+++ menu.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Crave Wave</title>
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" />
+
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <section id="header">
+            <a href="#"><img src="img/logoo3.png" class="logo" alt=""></a>
+            <div>
+                <ul id="navbar">
+                    <li><a href="index1.html">Home</a></li>
+                    <li><a class="active" href="menu.html">Menu</a></li>
+                    <li><a href="about.html">About</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                    <li><i class="far fa-shopping-bag" id="cart-icon"></i></li>
+                </ul>
+            </div>
+        </section>
+
+        <section id="page-header">
+            <h2>Delicious healthy meals at your doorstep</h2>
+            
+        </section>
+        
+        <section id="product1" class="section-p1">
+            <h1>VEG</h1>
+            <div class="pro-container">
+                <div class="pro" onclick="window.location.href='sandwich.html';">
+                    <img src="img/sandwich.png" alt="">
+                    <div class="des">
+                        <h5>Veg Grilled Sandwich</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rs.100</h4>
+                    </div>
+                    <!-- <a href="#"><i class="fal fa-shopping-cart cart"></i></a> -->
+                </div>
+                <div class="pro" onclick="window.location.href='paneertikka.html';">
+                    <img src="img/paneertikka.png" alt="">
+                    <div class="des">
+                        <h5>Paneer Tikka Masala</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rs.100</h4>
+                    </div>
+                    <!-- <a href="#"><i class="fal fa-shopping-cart cart"></i></a> -->
+                </div>
+                <div class="pro" onclick="window.location.href='chickpeasalad.html';">
+                    <img src="img/chickpea.png" alt="">
+                    <div class="des">
+                        <h5> CHICKPEA SALAD</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rs.100</h4>
+                    </div>
+                    <!-- <a href="#"><i class="fal fa-shopping-cart cart"></i></a> -->
+                </div>
+                <div class="pro" onclick="window.location.href='soyasalad.html';">
+                    <img src="img/soyasalad.png" alt="">
+                    <div class="des">
+                        <h5>SOYA CHUNKS SALAD</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rs.100</h4>
+                    </div>
+                    <!-- <a href="#"><i class="fal fa-shopping-cart cart"></i></a> -->
+                </div>
+                
+            </div>
+
+
+            <h1>NON-VEG</h1>
+            <div class="pro-container">
+                <div class="pro" onclick="window.location.href='biryani.html';">
+                    <img src="img/biryani.png" alt="">
+                    <div class="des">
+                        <h5>BIRYANI</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rs.100</h4>
+                    </div>
+                    <!-- <a href="#"><i class="fal fa-shopping-cart cart"></i></a> -->
+                </div>
+                <div class="pro" onclick="window.location.href='shawarma.html';">
+                    <img src="img/shawarma.png" alt="">
+                    <div class="des">
+                        <h5>SHAWARMA</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rs.100</h4>
+                    </div>
+                    <!-- <a href="#"><i class="fal fa-shopping-cart cart"></i></a> -->
+                </div>
+                <div class="pro" onclick="window.location.href='cnr.html';">
+                    <img src="img/cnr.png" alt="">
+                    <div class="des">
+                        <h5>CHICKEN AND RICE</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rs.100</h4>
+                    </div>
+                    <!-- <a href="#"><i class="fal fa-shopping-cart cart"></i></a> -->
+                </div>
+
+                <div class="pro" onclick="window.location.href='bc.html';">
+                    <img src="img/butter.png" alt="">
+                    <div class="des">
+                        <h5>BUTTER CHICKEN</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rs.100</h4>
+                    </div>
+                    <!-- <a href="#"><i class="fal fa-shopping-cart cart"></i></a> -->
+                </div>
+            </div>
+
+            <h1>Meal boxes</h1>
+            <div class="pro-container">
+                <div class="pro" onclick="window.location.href='meal1.html';">
+                    <img src="img/meal1.png" alt="">
+                    <div class="des">
+                        <h5>Meal 1</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rs.100</h4>
+                    </div>
+                    <!-- <a href="#"><i class="fal fa-shopping-cart cart"></i></a> -->
+                </div>
+                <div class="pro" onclick="window.location.href='meal2.html';">
+                    <img src="img/meal2.png" alt="">
+                    <div class="des">
+                        <h5>Meal 2</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rs.100</h4>
+                    </div>
+                    <!-- <a href="#"><i class="fal fa-shopping-cart cart"></i></a> -->
+                </div>
+                <div class="pro" onclick="window.location.href='meal3.html';">
+                    <img src="img/meal3.png" alt="">
+                    <div class="des">
+                        <h5>Meal 3</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rs.100</h4>
+                    </div>
+                    <!-- <a href="#"><i class="fal fa-shopping-cart cart"></i></a> -->
+                </div>
+
+                <div class="pro" onclick="window.location.href='meal4.html';">
+                    <img src="img/meal4.png" alt="">
+                    <div class="des">
+                        <h5>Meal 4</h5>
+                        <div class="star">
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                            <i class="fas fa-star"></i>
+                        </div>
+                        <h4>Rs.100</h4>
+                    </div>
+                    <!-- <a href="#"><i class="fal fa-shopping-cart cart"></i></a> -->
+                </div>
+            </div>
+
+        </section>
+        
+
+        <section id="pageno" class="section-p1">
+            <a href="#">1</a>
+            <a href="#">2</a>
+            <a href="#"><i class="fal fa-long-arrow-alt-right"></i></a>
+        </section>
+  
+        <footer class="section-p1">
+            <div class="col">
+                <img  class="logo" src="img/logoo3.png" alt="">
+                <h4>Contact</h4>
+                <p><strong>Address:</strong>  Nirmal Nagar Police Station Marg, Nirmal Nagar, Bandra East, Mumbai, Maharashtra 400051</p>
+                <p><strong>Phone:</strong>   022 29647361</p>
+                <p><strong>Hours:</strong>   Everyday: 10:00am-11:00pm</p>
+                <div class="follow">
+                    <h4>Follow Us</h4>
+                    <div class="icon">
+                        <i class="fab fa-facebook-f"></i>
+                        <i class="fab fa-twitter"></i>
+                        <i class="fab fa-instagram"></i>
+                        <i class="fab fa-youtube"></i>
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col">
+                <h4>About Us</h4>
+                <a href="#">About Us</a>
+                <a href="#">Privacy Policy</a>
+                <a href="#">Terms & Conditions</a>
+                <a href="#">Contact Us</a>
+            </div>
+
+
+            <div class="col">
+                <h4>My Account</h4>
+                <a href="#">Sign in</a>
+                <a href="#">View Cart</a>
+                <a href="#">Track my order</a>
+                <a href="#">Help</a>
+            </div>
+
+            <div class="col">
+                <h4> Payments</h4>
+                <img src="img/payment.png" alt="">
+            </div>
+            
+        </footer>
+
+
+        <script src="main.js"></script>
+    </body>
+
+</html>
